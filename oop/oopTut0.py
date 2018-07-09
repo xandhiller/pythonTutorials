0bbfc6d2dc75330075d707b22f56f18efd406ec8
xandhiller <xandhiller@gmail.com>
2018-07-09 16:22:23 +0400

    Class attributes vs instance attributes examined. Class attribute acted upon by a class method. Namespace printing learned as well.

--- oop/oopTut0.py
+++ oop/oopTut0.py
@@ -4,12 +4,14 @@
 # Self must be supplied to all methods of the class.
 class Employee:
     raise_amount = 1.05
+    num_of_emps = 0 # Initial value
 
     # The class gives the instance of the object as the first argument.
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
+        Employee.num_of_emps += 1 # Increment the number of employees each time a new class instance is initialised.
 
     def fullname(self):
         return ('{} {}'.format(self.first, self.last))
@@ -39,7 +41,15 @@ print('Class raise_amount is: ' + str(Employee.raise_amount))
 print('Variable 1 raise_amount is: ' + str(emp_1.raise_amount))
 print('Variable 2 raise_amount is: ' + str(emp_2.raise_amount))
 
-Employee.raise_amount = 1.06
-print('Class raise_amount is: ' + str(Employee.raise_amount))
-print('Variable 1 raise_amount is: ' + str(emp_1.raise_amount))
-print('Variable 2 raise_amount is: ' + str(emp_2.raise_amount))
+# Employee.raise_amount = 1.06
+# print('Class raise_amount is: ' + str(Employee.raise_amount))
+# print('Variable 1 raise_amount is: ' + str(emp_1.raise_amount))
+# print('Variable 2 raise_amount is: ' + str(emp_2.raise_amount))
+
+# So the conclusion is that class attributes kind of act like default values. If you change them, you change the attributes of all those objects of the class, but if you change only one locally then it will store that in its name space
+
+# You can print the namespace of a class instance by typing:
+# print(instance.__dict__), e.g.
+print(emp_1.__dict__)
+print(emp_2.__dict__)
+print(Employee.__dict__)
